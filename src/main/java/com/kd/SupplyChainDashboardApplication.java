95a639d58b60698353f7c40056892c63d223b622
dkapil97 <kapildhakade1902@gmail.com>
2025-09-20 16:04:56 +0530

    Initial commit in Supply Chain Dashboard

---
+++ src/main/java/com/kd/SupplyChainDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.kd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplyChainDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupplyChainDashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kd/SupplyChainDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupplyChainDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
